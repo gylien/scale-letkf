e579f16b4a707f9b9ef009c6ea042298fd22ff26
Guo-Yuan Lien [K] <guo-yuan.lien@riken.jp>
2016-04-06 12:02:03 +0900

    Add 'realtime_v160405_d1' configuration files and revise 'admin.sh' file for real-time jobs
    -- Initially add 'example' configuration files

--- scale/run/admin.sh
+++ scale/run/admin.sh
@@ -24,7 +24,7 @@ WTIME_L="$6"
 
 #ETIME=$(datetime "$STIME" ${LCYCLE} s)
 
-CONFDIR="$OUTDIR/${STIME}/${SCPNAME}_conf"
+CONFIG='realtime_v160405_d1'
 
 #-------------------------------------------------------------------------------
 
@@ -33,23 +33,20 @@ if [ "$SCPNAME" = 'cycle' ]; then
 else
   STIME_DIR="${STIME}"
 fi
-cat config/EastAsia_18km_48p/config.main.K | \
+cat config/${CONFIG}/config.main.K | \
     sed -e "s/<STIME>/${STIME_DIR}/g" | \
     sed -e "s/<NNODES>/${NNODES}/g" \
     > config.main
 
-cat config/EastAsia_18km_48p/config.${SCPNAME} | \
+cat config/${CONFIG}/config.${SCPNAME} | \
     sed -e "s/<STIME>/${STIME}/g" | \
     sed -e "s/<WTIME_L>/${WTIME_L}/g" \
     > config.${SCPNAME}
 
-cat config/EastAsia_18km_48p/config.nml.scale | \
+cat config/${CONFIG}/config.nml.scale | \
     sed -e "s/<TIME_DT>/${TIME_DT}/g" | \
     sed -e "s/<TIME_DT_DYN>/${TIME_DT_DYN}/g" \
     > config.nml.scale
-
-mkdir -p $CONFDIR
-cp config.* $CONFDIR
 
 #-------------------------------------------------------------------------------
 
@@ -59,37 +56,31 @@ res=$? && ((res != 0)) && exit $res
 jobname="${SCPNAME}_${SYSNAME}"
 jobid=$(grep 'pjsub Job' ${SCPNAME}_K.log | cut -d ' ' -f6)
 
-n=0
-nmax=120
-while [ ! -s "${jobname}.o${jobid}" ] || [ ! -s "${jobname}.e${jobid}" ] ||
-      [ ! -s "${jobname}.s${jobid}" ] || [ ! -s "${jobname}.i${jobid}" ] && ((n < nmax)); do
-  n=$((n+1))
-  sleep 5s
-done
-
 #-------------------------------------------------------------------------------
 
-res=0
-if ((n >= nmax)); then
-  res=101
+if [ ! -s "${jobname}.o${jobid}" ] || [ ! -s "${jobname}.e${jobid}" ] || \
+   [ ! -s "${jobname}.i${jobid}" ] || [ ! -s "${jobname}.s${jobid}" ]; then
+  exit 101
 elif [ -n "$(grep 'ERR.' ${jobname}.e${jobid})" ]; then
-  res=102
+  exit 102
 elif [ -n "$(grep 'terminated' ${jobname}.e${jobid})" ]; then
-  res=103
-elif [ ! -s "${jobname}.s${jobid}" ]; then
-  res=104
-elif [ "$(tail -n 1 ${jobname}.s${jobid})" != "---(Stage-Out Error Information)---" ]; then
-  res=105
+  exit 103
+#elif [ ! -s "${jobname}.s${jobid}" ]; then
+#  exit 104
+#elif [ "$(tail -n 1 ${jobname}.s${jobid})" != "---(Stage-Out Error Information)---" ]; then
+#  exit 105
 fi
 
-mv -f ${SCPNAME}_job.sh $CONFDIR/${jobid}.b
-mv -f ${SCPNAME}_K.log $CONFDIR/${jobid}.l
+rm -f ${SCPNAME}_job.sh
+rm -f ${jobname}.o${jobid}
+rm -f ${jobname}.e${jobid}
+rm -f ${jobname}.s${jobid}
+rm -f ${jobname}.i${jobid}
 
-mv -f ${jobname}.o${jobid} $CONFDIR/${jobid}.o
-mv -f ${jobname}.e${jobid} $CONFDIR/${jobid}.e
-mv -f ${jobname}.s${jobid} $CONFDIR/${jobid}.s
-mv -f ${jobname}.i${jobid} $CONFDIR/${jobid}.i
+mkdir -p exp
+rm -f exp/*
+ln -s $OUTDIR/exp/${jobid}_${SCPNAME}_${STIME} exp
 
 #-------------------------------------------------------------------------------
 
-exit $res
+exit 0
