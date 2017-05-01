gylien/scale-letkf

commit e558c81725c05446ef8e8bad56aa86a6bbeac2d8

Write analysis ensemble mean and members in parallel in "write_ens_mpi' subroutine Many other revisions to improve the code efficiency and readability: A)  -- Clean up and fix the code style of 'common_mpi_scale.f90' and 'common_scale.f90' files B)  -- Split the 'write_ensmspr_mpi' subroutine into 'write_ensmean' and 'write_enssprd',     and 'enssprd_grd' (in 'common_scale.f90') subroutines  -- Implement optional ensemble mean calculation and observation departure monitor functions     in 'write_ensmean' subroutine  -- Modify 'test.sh' to adapt to the slight different LETKF output message  -- Add 'read_ens_mpi_addiinfl' subroutine for ensemble inputs of only members without mean C)  -- Re-design the namelist variables related to ensemble I/O file names:     Add: 'OBSDA_MEAN_OUT_BASENAME'          'OBSDA_MDET_OUT_BASENAME'          'HISTORY_MEAN_IN_BASENAME'          'HISTORY_MDET_IN_BASENAME'          'OBSDA_MEAN_IN_BASENAME'          'OBSDA_MDET_IN_BASENAME'          'GUES_MDET_IN_BASENAME'          'GUES_SPRD_OUT'          'ANAL_MDET_OUT_BASENAME'          'ANAL_SPRD_OUT'     Rename: 'GUES_OUT_MEAN_BASENAME' -> 'GUES_MEAN_INOUT_BASENAME'             'GUES_OUT_SPRD_BASENAME' -> 'GUES_SPRD_OUT_BASENAME'             'ANAL_OUT_MEAN_BASENAME' -> 'ANAL_MEAN_OUT_BASENAME'             'ANAL_OUT_SPRD_BASENAME' -> 'ANAL_SPRD_OUT_BASENAME'  -- Properly set the default values of the above namelist variables, so that all of them     do not need to be specified usually  -- Add 'memf_{mean,mdet,sprd}' parameters and revise the 'file_member_replace' subroutine     in 'common_nml.f90' D)  -- Rename 'obsda2' to 'obsda_sort' for clarity  -- Move 'obs' and 'obsda_sort' global variables from 'letkf_obs.f90' to 'common_obs_scale.f90'     to enlarge the accessiable scope of the variables  -- Rename variable name from 'obs' to 'obsda' in some subroutines in 'common_obs_scale.f90'     to present confusion E)  -- Improve the timer measurement and outputs  -- Remove unnecessary MPI_BARRIER

scale/run/config/BDA_d1_15km_48p_bf10/config.nml.letkf
@@ -20,11 +20,9 @@
 !--OBSDA_IN--
 !--OBSDA_IN_BASENAME--
 !--GUES_IN_BASENAME--
-!--GUES_OUT_MEAN_BASENAME--
-!--GUES_OUT_SPRD_BASENAME--
+!--GUES_SPREAD_OUT--
 !--ANAL_OUT_BASENAME--
-!--ANAL_OUT_MEAN_BASENAME--
-!--ANAL_OUT_SPRD_BASENAME--
+!--ANAL_SPREAD_OUT--
 !--LETKF_TOPO_IN_BASENAME--
  SIGMA_OBS = 400.0D3,
  SIGMA_OBSV = 0.3D0,

scale/run/config/BDA_d2_5km_64p_bf16/config.nml.letkf
@@ -20,11 +20,9 @@
 !--OBSDA_IN--
 !--OBSDA_IN_BASENAME--
 !--GUES_IN_BASENAME--
-!--GUES_OUT_MEAN_BASENAME--
-!--GUES_OUT_SPRD_BASENAME--
+!--GUES_SPREAD_OUT--
 !--ANAL_OUT_BASENAME--
-!--ANAL_OUT_MEAN_BASENAME--
-!--ANAL_OUT_SPRD_BASENAME--
+!--ANAL_SPREAD_OUT--
 !--LETKF_TOPO_IN_BASENAME--
  SIGMA_OBS = 400.0D3,
  SIGMA_OBSV = 0.3D0,

scale/run/config/BDA_d3_100m_256p_bf40/config.nml.letkf
@@ -20,11 +20,9 @@
 !--OBSDA_IN--
 !--OBSDA_IN_BASENAME--
 !--GUES_IN_BASENAME--
-!--GUES_OUT_MEAN_BASENAME--
-!--GUES_OUT_SPRD_BASENAME--
+!--GUES_SPREAD_OUT--
 !--ANAL_OUT_BASENAME--
-!--ANAL_OUT_MEAN_BASENAME--
-!--ANAL_OUT_SPRD_BASENAME--
+!--ANAL_SPREAD_OUT--
 !--LETKF_TOPO_IN_BASENAME--
  COV_INFL_MUL = 1.00D0,
 ! MIN_INFL_MUL = 0.0D0,

scale/run/config/BDA_d3_100m_64p_bf40/config.nml.letkf
@@ -20,11 +20,9 @@
 !--OBSDA_IN--
 !--OBSDA_IN_BASENAME--
 !--GUES_IN_BASENAME--
-!--GUES_OUT_MEAN_BASENAME--
-!--GUES_OUT_SPRD_BASENAME--
+!--GUES_SPREAD_OUT--
 !--ANAL_OUT_BASENAME--
-!--ANAL_OUT_MEAN_BASENAME--
-!--ANAL_OUT_SPRD_BASENAME--
+!--ANAL_SPREAD_OUT--
 !--LETKF_TOPO_IN_BASENAME--
  COV_INFL_MUL = 1.00D0,
 ! MIN_INFL_MUL = 0.0D0,

scale/run/config/BDA_d3_100m_720p_bf40/config.nml.letkf
@@ -20,11 +20,9 @@
 !--OBSDA_IN--
 !--OBSDA_IN_BASENAME--
 !--GUES_IN_BASENAME--
-!--GUES_OUT_MEAN_BASENAME--
-!--GUES_OUT_SPRD_BASENAME--
+!--GUES_SPREAD_OUT--
 !--ANAL_OUT_BASENAME--
-!--ANAL_OUT_MEAN_BASENAME--
-!--ANAL_OUT_SPRD_BASENAME--
+!--ANAL_SPREAD_OUT--
 !--LETKF_TOPO_IN_BASENAME--
  INFL_MUL = 1.00D0,
 ! INFL_MUL_MIN = 0.0D0,

scale/run/config/BDA_d3_1km_9p_bf20/config.nml.letkf
@@ -20,11 +20,9 @@
 !--OBSDA_IN--
 !--OBSDA_IN_BASENAME--
 !--GUES_IN_BASENAME--
-!--GUES_OUT_MEAN_BASENAME--
-!--GUES_OUT_SPRD_BASENAME--
+!--GUES_SPREAD_OUT--
 !--ANAL_OUT_BASENAME--
-!--ANAL_OUT_MEAN_BASENAME--
-!--ANAL_OUT_SPRD_BASENAME--
+!--ANAL_SPREAD_OUT--
 !--LETKF_TOPO_IN_BASENAME--
  INFL_MUL = 1.00D0,
 ! INFL_MUL_MIN = 0.0D0,

scale/run/config/BDA_d4_100m_64p_bf40/config.nml.letkf
@@ -20,11 +20,9 @@
 !--OBSDA_IN--
 !--OBSDA_IN_BASENAME--
 !--GUES_IN_BASENAME--
-!--GUES_OUT_MEAN_BASENAME--
-!--GUES_OUT_SPRD_BASENAME--
+!--GUES_SPREAD_OUT--
 !--ANAL_OUT_BASENAME--
-!--ANAL_OUT_MEAN_BASENAME--
-!--ANAL_OUT_SPRD_BASENAME--
+!--ANAL_SPREAD_OUT--
 !--LETKF_TOPO_IN_BASENAME--
  INFL_MUL = 1.00D0,
 ! INFL_MUL_MIN = 0.0D0,

scale/run/config/BDA_d4_1km_4p_bf20/config.nml.letkf
@@ -20,11 +20,9 @@
 !--OBSDA_IN--
 !--OBSDA_IN_BASENAME--
 !--GUES_IN_BASENAME--
-!--GUES_OUT_MEAN_BASENAME--
-!--GUES_OUT_SPRD_BASENAME--
+!--GUES_SPREAD_OUT--
 !--ANAL_OUT_BASENAME--
-!--ANAL_OUT_MEAN_BASENAME--
-!--ANAL_OUT_SPRD_BASENAME--
+!--ANAL_SPREAD_OUT--
 !--LETKF_TOPO_IN_BASENAME--
  INFL_MUL = 1.00D0,
 ! INFL_MUL_MIN = 0.0D0,

commit d757fc67f7d25982fa4011571cf8e3c5d0cc7a1b

Fix a bug of wrong 'gues/mean' and 'gues/sprd' outputs, introduced by Commit e558c81

scale/run/config/BDA_d1_15km_48p_bf10/config.nml.letkf
@@ -21,6 +21,8 @@
 !--OBSDA_IN--
 !--OBSDA_IN_BASENAME--
 !--GUES_IN_BASENAME--
+!--GUES_MEAN_INOUT_BASENAME--
+!--GUES_SPRD_OUT_BASENAME--
 !--GUES_SPRD_OUT--
 !--ANAL_OUT_BASENAME--
 !--ANAL_SPRD_OUT--

scale/run/config/BDA_d2_5km_64p_bf16/config.nml.letkf
@@ -21,6 +21,8 @@
 !--OBSDA_IN--
 !--OBSDA_IN_BASENAME--
 !--GUES_IN_BASENAME--
+!--GUES_MEAN_INOUT_BASENAME--
+!--GUES_SPRD_OUT_BASENAME--
 !--GUES_SPRD_OUT--
 !--ANAL_OUT_BASENAME--
 !--ANAL_SPRD_OUT--

scale/run/config/BDA_d3_100m_256p_bf40/config.nml.letkf
@@ -21,6 +21,8 @@
 !--OBSDA_IN--
 !--OBSDA_IN_BASENAME--
 !--GUES_IN_BASENAME--
+!--GUES_MEAN_INOUT_BASENAME--
+!--GUES_SPRD_OUT_BASENAME--
 !--GUES_SPRD_OUT--
 !--ANAL_OUT_BASENAME--
 !--ANAL_SPRD_OUT--

scale/run/config/BDA_d3_100m_64p_bf40/config.nml.letkf
@@ -21,6 +21,8 @@
 !--OBSDA_IN--
 !--OBSDA_IN_BASENAME--
 !--GUES_IN_BASENAME--
+!--GUES_MEAN_INOUT_BASENAME--
+!--GUES_SPRD_OUT_BASENAME--
 !--GUES_SPRD_OUT--
 !--ANAL_OUT_BASENAME--
 !--ANAL_SPRD_OUT--

scale/run/config/BDA_d3_100m_720p_bf40/config.nml.letkf
@@ -21,6 +21,8 @@
 !--OBSDA_IN--
 !--OBSDA_IN_BASENAME--
 !--GUES_IN_BASENAME--
+!--GUES_MEAN_INOUT_BASENAME--
+!--GUES_SPRD_OUT_BASENAME--
 !--GUES_SPRD_OUT--
 !--ANAL_OUT_BASENAME--
 !--ANAL_SPRD_OUT--

scale/run/config/BDA_d3_1km_9p_bf20/config.nml.letkf
@@ -21,6 +21,8 @@
 !--OBSDA_IN--
 !--OBSDA_IN_BASENAME--
 !--GUES_IN_BASENAME--
+!--GUES_MEAN_INOUT_BASENAME--
+!--GUES_SPRD_OUT_BASENAME--
 !--GUES_SPRD_OUT--
 !--ANAL_OUT_BASENAME--
 !--ANAL_SPRD_OUT--

scale/run/config/BDA_d4_100m_64p_bf40/config.nml.letkf
@@ -27,6 +27,8 @@
 !--OBSDA_IN--
 !--OBSDA_IN_BASENAME--
 !--GUES_IN_BASENAME--
+!--GUES_MEAN_INOUT_BASENAME--
+!--GUES_SPRD_OUT_BASENAME--
 !--GUES_SPRD_OUT--
 !--ANAL_OUT_BASENAME--
 !--ANAL_SPRD_OUT--

scale/run/config/BDA_d4_1km_4p_bf20/config.nml.letkf
@@ -21,6 +21,8 @@
 !--OBSDA_IN--
 !--OBSDA_IN_BASENAME--
 !--GUES_IN_BASENAME--
+!--GUES_MEAN_INOUT_BASENAME--
+!--GUES_SPRD_OUT_BASENAME--
 !--GUES_SPRD_OUT--
 !--ANAL_OUT_BASENAME--
 !--ANAL_SPRD_OUT--

scale/run/config/BDA_d4_250m_36p_bf40/config.nml.letkf
@@ -21,6 +21,8 @@
 !--OBSDA_IN--
 !--OBSDA_IN_BASENAME--
 !--GUES_IN_BASENAME--
+!--GUES_MEAN_INOUT_BASENAME--
+!--GUES_SPRD_OUT_BASENAME--
 !--GUES_SPRD_OUT--
 !--ANAL_OUT_BASENAME--
 !--ANAL_SPRD_OUT--

scale/run/config/BDA_d4_500m_9p_bf30/config.nml.letkf
@@ -21,6 +21,8 @@
 !--OBSDA_IN--
 !--OBSDA_IN_BASENAME--
 !--GUES_IN_BASENAME--
+!--GUES_MEAN_INOUT_BASENAME--
+!--GUES_SPRD_OUT_BASENAME--
 !--GUES_SPRD_OUT--
 !--ANAL_OUT_BASENAME--
 !--ANAL_SPRD_OUT--

scale/run/config/example/config.nml.letkf
@@ -27,6 +27,8 @@
 !--OBSDA_IN--
 !--OBSDA_IN_BASENAME--
 !--GUES_IN_BASENAME--
+!--GUES_MEAN_INOUT_BASENAME--
+!--GUES_SPRD_OUT_BASENAME--
 !--GUES_SPRD_OUT--
 !--ANAL_OUT_BASENAME--
 !--ANAL_SPRD_OUT--

scale/run/config/realtime_r0020_d1/config.nml.letkf
@@ -20,6 +20,8 @@
 !--OBSDA_IN--
 !--OBSDA_IN_BASENAME--
 !--GUES_IN_BASENAME--
+!--GUES_MEAN_INOUT_BASENAME--
+!--GUES_SPRD_OUT_BASENAME--
 !--GUES_OUT_MEAN_BASENAME--
 !--GUES_OUT_SPRD_BASENAME--
 !--ANAL_OUT_BASENAME--

scale/run/config/realtime_r0023_d1/config.nml.letkf
@@ -22,6 +22,8 @@
 !--OBSDA_IN--
 !--OBSDA_IN_BASENAME--
 !--GUES_IN_BASENAME--
+!--GUES_MEAN_INOUT_BASENAME--
+!--GUES_SPRD_OUT_BASENAME--
 !--GUES_SPRD_OUT--
 !--ANAL_OUT_BASENAME--
 !--ANAL_SPRD_OUT--

scale/run/config/realtime_v160405_d1/config.nml.letkf
@@ -9,6 +9,8 @@
  OBS_IN_FORMAT = 1, 2, 3,
 !--OBSDA_IN_BASENAME--
 !--GUES_IN_BASENAME--
+!--GUES_MEAN_INOUT_BASENAME--
+!--GUES_SPRD_OUT_BASENAME--
 !--GUES_SPRD_OUT--
 !--ANAL_OUT_BASENAME--
 !--ANAL_SPRD_OUT--

scale/run/config/testcase_15km_48p_l36/config.nml.letkf
@@ -21,6 +21,8 @@
 !--OBSDA_IN--
 !--OBSDA_IN_BASENAME--
 !--GUES_IN_BASENAME--
+!--GUES_MEAN_INOUT_BASENAME--
+!--GUES_SPRD_OUT_BASENAME--
 !--GUES_SPRD_OUT--
 !--ANAL_OUT_BASENAME--
 !--ANAL_SPRD_OUT--

scale/run/config/testcase_45km_4p_l36/config.nml.letkf
@@ -22,6 +22,8 @@
 !--OBSDA_IN--
 !--OBSDA_IN_BASENAME--
 !--GUES_IN_BASENAME--
+!--GUES_MEAN_INOUT_BASENAME--
+!--GUES_SPRD_OUT_BASENAME--
 !--GUES_SPRD_OUT--
 !--ANAL_OUT_BASENAME--
 !--ANAL_SPRD_OUT--

scale/run/config/testcase_45km_4p_l36_bdyprep/config.nml.letkf
@@ -21,6 +21,8 @@
 !--OBSDA_IN--
 !--OBSDA_IN_BASENAME--
 !--GUES_IN_BASENAME--
+!--GUES_MEAN_INOUT_BASENAME--
+!--GUES_SPRD_OUT_BASENAME--
 !--GUES_SPRD_OUT--
 !--ANAL_OUT_BASENAME--
 !--ANAL_SPRD_OUT--

scale/run/config/testcase_45km_4p_l36_d2/config.nml.letkf
@@ -21,6 +21,8 @@
 !--OBSDA_IN--
 !--OBSDA_IN_BASENAME--
 !--GUES_IN_BASENAME--
+!--GUES_MEAN_INOUT_BASENAME--
+!--GUES_SPRD_OUT_BASENAME--
 !--GUES_SPRD_OUT--
 !--ANAL_OUT_BASENAME--
 !--ANAL_SPRD_OUT--

scale/run/config/testcase_45km_4p_l36_gfsinput/config.nml.letkf
@@ -21,6 +21,8 @@
 !--OBSDA_IN--
 !--OBSDA_IN_BASENAME--
 !--GUES_IN_BASENAME--
+!--GUES_MEAN_INOUT_BASENAME--
+!--GUES_SPRD_OUT_BASENAME--
 !--GUES_SPRD_OUT--
 !--ANAL_OUT_BASENAME--
 !--ANAL_SPRD_OUT--

scale/run/config/testcase_45km_4p_l72/config.nml.letkf
@@ -21,6 +21,8 @@
 !--OBSDA_IN--
 !--OBSDA_IN_BASENAME--
 !--GUES_IN_BASENAME--
+!--GUES_MEAN_INOUT_BASENAME--
+!--GUES_SPRD_OUT_BASENAME--
 !--GUES_SPRD_OUT--
 !--ANAL_OUT_BASENAME--
 !--ANAL_SPRD_OUT--

scale/run/config/testcase_PAWR_1km_4p/config.nml.letkf
@@ -21,6 +21,8 @@
 !--OBSDA_IN--
 !--OBSDA_IN_BASENAME--
 !--GUES_IN_BASENAME--
+!--GUES_MEAN_INOUT_BASENAME--
+!--GUES_SPRD_OUT_BASENAME--
 !--GUES_SPRD_OUT--
 !--ANAL_OUT_BASENAME--
 !--ANAL_SPRD_OUT--

scale/run/config/testcase_PAWR_5km_4p/config.nml.letkf
@@ -27,6 +27,8 @@
 !--OBSDA_IN--
 !--OBSDA_IN_BASENAME--
 !--GUES_IN_BASENAME--
+!--GUES_MEAN_INOUT_BASENAME--
+!--GUES_SPRD_OUT_BASENAME--
 !--GUES_SPRD_OUT--
 !--ANAL_OUT_BASENAME--
 !--ANAL_SPRD_OUT--

scale/run/src/pre_letkf_node.sh
@@ -119,6 +119,8 @@ cat $TMPDAT/conf/config.nml.letkf | \
         -e "/!--OBSDA_IN--/a OBSDA_IN = $OBSDA_IN," \
         -e "/!--OBSDA_IN_BASENAME--/a OBSDA_IN_BASENAME = \"${TMPOUT}/${ATIME}/obsgues/@@@@/obsda.ext\"," \
         -e "/!--GUES_IN_BASENAME--/a GUES_IN_BASENAME = \"${TMPOUT}/${ATIME}/anal/@@@@/init\"," \
+        -e "/!--GUES_MEAN_INOUT_BASENAME--/a GUES_MEAN_INOUT_BASENAME = \"${TMPOUT}/${ATIME}/gues/mean/init\"," \
+        -e "/!--GUES_SPRD_OUT_BASENAME--/a GUES_SPRD_OUT_BASENAME = \"${TMPOUT}/${ATIME}/gues/sprd/init\"," \
         -e "/!--GUES_SPRD_OUT--/a GUES_SPRD_OUT = ${SPRD_OUT_TF}," \
         -e "/!--ANAL_OUT_BASENAME--/a ANAL_OUT_BASENAME = \"${TMPOUT}/${ATIME}/anal/@@@@/init\"," \
         -e "/!--ANAL_SPRD_OUT--/a ANAL_SPRD_OUT = ${SPRD_OUT_TF}," \