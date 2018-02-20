338c93bb574f9a018109ab94732c8ff8145f2311
Guo-Yuan Lien <guo-yuan.lien@riken.jp>
2018-02-20 12:10:52 +0900

    Maintain backward compatibility for the original dynamic-config mode  -- Modify the dynamic-config scripts for the updated parallelization settings introduced in '907e6ca'

--- scale/run/src/pre_obsope_node.sh
+++ scale/run/src/pre_obsope_node.sh
@@ -77,10 +77,17 @@ fi
 
 #===============================================================================
 
-cat $TMPDAT/conf/config.nml.obsope | \
+cat $TMPDAT/conf/config.nml.ensmodel | \
     sed -e "/!--MEMBER--/a MEMBER = $MEMBER," \
         -e "/!--DET_RUN--/a DET_RUN = ${DET_RUN_TF}," \
-        -e "/!--OBS_IN_NUM--/a OBS_IN_NUM = $OBSNUM," \
+        -e "/!--PPN--/a PPN = $PPN_APPAR," \
+        -e "/!--MEM_NODES--/a MEM_NODES = $MEM_NODES," \
+        -e "/!--NUM_DOMAIN--/a NUM_DOMAIN = 1," \
+        -e "/!--PRC_DOMAINS--/a PRC_DOMAINS = $MEM_NP," \
+    > $TMPDIR/obsope.conf
+
+cat $TMPDAT/conf/config.nml.obsope | \
+    sed -e "/!--OBS_IN_NUM--/a OBS_IN_NUM = $OBSNUM," \
         -e "/!--OBS_IN_NAME--/a OBS_IN_NAME = $OBS_IN_NAME_LIST" \
         -e "/!--OBSDA_RUN--/a OBSDA_RUN = $OBSDA_RUN_LIST" \
         -e "/!--OBSDA_OUT--/a OBSDA_OUT = .true." \
@@ -90,12 +97,8 @@ cat $TMPDAT/conf/config.nml.obsope | \
         -e "/!--SLOT_END--/a SLOT_END = $SLOT_END," \
         -e "/!--SLOT_BASE--/a SLOT_BASE = $SLOT_BASE," \
         -e "/!--SLOT_TINTERVAL--/a SLOT_TINTERVAL = $LTIMESLOT.D0," \
-        -e "/!--NNODES--/a NNODES = $NNODES_APPAR," \
-        -e "/!--PPN--/a PPN = $PPN_APPAR," \
-        -e "/!--MEM_NODES--/a MEM_NODES = $MEM_NODES," \
-        -e "/!--MEM_NP--/a MEM_NP = $MEM_NP," \
         -e "/!--IO_AGGREGATE--/a IO_AGGREGATE = ${IO_AGGREGATE}," \
-    > $TMPDIR/obsope.conf
+    >> $TMPDIR/obsope.conf
 
 # Most of these parameters are not important for obsope
 cat $TMPDAT/conf/config.nml.scale | \
