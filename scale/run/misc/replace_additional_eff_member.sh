f5e5bd18efc5ea2084ee25fe97cf24e07718b656
Arata <arata.amemiya@riken.jp>
2020-03-31 01:12:36 +0900

    clean up

--- scale/run/misc/replace_additional_eff_member.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-
-list="$(grep -rn 'RESTART_OUT_ADDITIONAL_BASENAME' ../config | cut -d ':' -f1)"
-
-for i in $list; do
-  echo $i
-
-  sed -i '/RESTART_OUT_ADDITIONAL_BASENAME/a !--RESTART_OUT_ADDITIONAL_EFF_MEMBER--' $i
-done
