060b69b222f1e1b3d2c66197fb6a8ba4d5982042
Arata <arata.amemiya@riken.jp>
2020-04-23 00:57:46 +0900

    minor bug fix

--- external/past/get_past.sh
+++ external/past/get_past.sh
@@ -15,6 +15,7 @@ txtfile=$wkdir/../../admin/time_offset.txt
 
 lag_obs="6 hour"
 lag_gfs="4 hour"
+lag_msm="6 hour"
 
 if [ -f $txtfile ]; then
   time_offset=`cat $time_offset`
@@ -28,7 +29,13 @@ if [ -f $txtfile ]; then
   for gfsdir in `/bin/ls -xd $wkdir/ncepgfs_grads/*`;do
     gfstime=`date -ud "${gfsdir:0:4}-${gfsdir:4:2}-${gfsdir:6:2} ${gfsdir:8:2}"`
     if [ `date -ud "$gfstime" +%s` -lt `date -ud "- $lag_gfs $pasttime" +%s` ];then
-      mv $wkdir/ncepgfs_grads/$obstime $wkdir/../ncepgfs_grads/
+      mv $wkdir/ncepgfs_grads/$gfstime $wkdir/../ncepgfs_grads/
+    fi    
+  done
+  for msmdir in `/bin/ls -xd $wkdir/msm/*`;do
+    msmtime=`date -ud "${msmdir:0:4}-${msmdir:4:2}-${msmdir:6:2} ${msmdir:8:2}"`
+    if [ `date -ud "$msmtime" +%s` -lt `date -ud "- $lag_msm $pasttime" +%s` ];then
+      mv $wkdir/msm/$msmtime $wkdir/../msm/
     fi    
   done
 fi
