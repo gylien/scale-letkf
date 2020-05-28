b80dd16d4f8224f3da54e36c8beeb366fb8c9789
Arata <arata.amemiya@riken.jp>
2020-05-28 11:21:49 +0900

    fix get_past.sh

--- external/past/get_past.sh
+++ external/past/get_past.sh
@@ -24,21 +24,21 @@ if [ -f $txtfile ]; then
     obsdir=`basename $dir`
     obstime=`date -ud "${obsdir:0:4}-${obsdir:4:2}-${obsdir:6:2} ${obsdir:8:2}"`
     if [ `date -ud "$obstime" +%s` -lt `date -ud "- $lag_obs $pasttime" +%s` ];then
-      mv $wkdir/ncepobs_gdas_letkf/$obstime $wkdir/../ncepobs_gdas_letkf/
+      mv $wkdir/ncepobs_gdas_letkf/$obsdir $wkdir/../ncepobs_gdas_letkf/
     fi    
   done
   for dir in `/bin/ls -xd $wkdir/ncepgfs_grads/*`;do
     gfsdir=`basename $dir`
     gfstime=`date -ud "${gfsdir:0:4}-${gfsdir:4:2}-${gfsdir:6:2} ${gfsdir:8:2}"`
     if [ `date -ud "$gfstime" +%s` -lt `date -ud "- $lag_gfs $pasttime" +%s` ];then
-      mv $wkdir/ncepgfs_grads/$gfstime $wkdir/../ncepgfs_grads/
+      mv $wkdir/ncepgfs_grads/$gfsdir $wkdir/../ncepgfs_grads/
     fi    
   done
   for dir in `/bin/ls -xd $wkdir/msm/*`;do
     msmdir=`basename $dir`
     msmtime=`date -ud "${msmdir:0:4}-${msmdir:4:2}-${msmdir:6:2} ${msmdir:8:2}"`
     if [ `date -ud "$msmtime" +%s` -lt `date -ud "- $lag_msm $pasttime" +%s` ];then
-      mv $wkdir/msm/$msmtime $wkdir/../msm/
+      mv $wkdir/msm/$msmdir $wkdir/../msm/
     fi    
   done
 fi
